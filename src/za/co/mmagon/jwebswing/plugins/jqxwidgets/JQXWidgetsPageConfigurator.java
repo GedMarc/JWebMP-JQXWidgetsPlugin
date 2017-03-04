e3df8393d01a3fda89c0e89ba768abc2403ed09b
Marc Magon <ged_marc@hotmail.com>
2017-03-04 09:24:25 +0200

    v0.9

--- src/za/co/mmagon/jwebswing/plugins/jqxwidgets/JQXWidgetsPageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/jqxwidgets/JQXWidgetsPageConfigurator.java
@@ -1,10 +1,10 @@
-/*
- * Copyright (C) 2017 GedMarc
+/* 
+ * Copyright (C) 2017 Marc Magon
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package za.co.mmagon.jwebswing.plugins.jqxwidgets;
 
@@ -27,17 +26,23 @@ import za.co.mmagon.jwebswing.plugins.PluginInformation;
  * @since 28 Feb 2017
  *
  */
-@PluginInformation(pluginName = "jqWidgets", pluginUniqueName = "jqx-widgets-free",
-        pluginDescription = "QWidgets provides a comprehensive solution for building professional web sites and mobile apps. It is built entirely on open standards and technologies like HTML5, CSS, JavaScript and jQuery. ",
-        pluginVersion = "4.5.0",
-        pluginDependancyUniqueIDs = "jquery,globalize",
-        pluginCategories = "jquery, ui, jqwidgets, jqxwidgets, framework, web",
-        pluginSubtitle = "We specialize in development of platform independent, mobile-ready user interface components and presentation layer for responsive websites and mobile apps.",
-        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-JQXWidgetsPlugin",
-        pluginSourceUrl = "http://www.jqwidgets.com/",
-        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-JQXWidgetsPlugin/wiki",
-        pluginOriginalHomepage = "http://www.jqwidgets.com/",
-        pluginCommercial = true)
+@PluginInformation(pluginName = "jqWidgets",
+                   pluginUniqueName = "jqx-widgets-free",
+                   pluginDescription = "JQWidgets provides a comprehensive solution for building professional web sites and mobile apps. It is built entirely on open standards and technologies like HTML5, CSS, JavaScript and jQuery. ",
+                   pluginVersion = "4.5.0",
+                   pluginDependancyUniqueIDs = "jquery,globalize",
+                   pluginCategories = "jquery, ui, jqwidgets, jqxwidgets, framework, web",
+                   pluginSubtitle = "We specialize in development of platform independent, mobile-ready user interface components and presentation layer for responsive websites and mobile apps.",
+                   pluginGitUrl = "https://github.com/GedMarc/JWebSwing-JQXWidgetsPlugin",
+                   pluginSourceUrl = "http://www.jqwidgets.com/",
+                   pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-JQXWidgetsPlugin/wiki",
+                   pluginOriginalHomepage = "http://www.jqwidgets.com/",
+                   pluginCommercial = true,
+                   pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/JQXWidgetsPlugin.jar/download",
+                   pluginIconUrl = "bower_components/jqwidgets/logo-jqwidgets.svg",
+                   pluginIconImageUrl = "bower_components/jqwidgets/jqwidgets_example.png",
+                   pluginLastUpdatedDate = "2017/03/04"
+)
 public class JQXWidgetsPageConfigurator extends PageConfigurator
 {
 

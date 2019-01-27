5dfa783f3858921830a16877d2e885031afaf2f6
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:14:03 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,8 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.jqxwidgets.JQXWidgetsPageConfigurator;
-
 open module com.jwebmp.plugins.jqxwidgets {
 
 	exports com.jwebmp.plugins.jqxwidgets;
@@ -20,8 +15,8 @@ open module com.jwebmp.plugins.jqxwidgets {
 	requires com.jwebmp.plugins.globalize.cultures;
 	requires com.jwebmp.core.angularjs;
 
-	provides IPageConfigurator with JQXWidgetsPageConfigurator;
-	provides IGuiceScanModuleExclusions with com.jwebmp.plugins.jqxwidgets.implementations.JQXWidgetsExclusionsModule;
-	provides IGuiceScanJarExclusions with com.jwebmp.plugins.jqxwidgets.implementations.JQXWidgetsExclusionsModule;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.jqxwidgets.JQXWidgetsPageConfigurator;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.jqxwidgets.implementations.JQXWidgetsExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.jqxwidgets.implementations.JQXWidgetsExclusionsModule;
 
 }
